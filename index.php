8d0021c8c802e549638e4731ade791faf2589394
Dave607 <dave_607@outlook.com>
2017-06-12 10:12:13 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>mike.ioe </title>
+   <link rel="stylesheet" href="css/custom.css">
+	    <style>
+
+
+</style>
+
+</head>
+
+<body>
+<center>
+<a href="http://10.183.1.13:8080" class = "slink" >:8080</a><a href="http://10.183.1.13:3000" class = "slink" >:3000</a><a href="downloads/" class = "slink" >downloads</a><a href="qnb/"class = "slink" >pma</a><a href="logs"class = "slink" >logs</a><a href="docs/" class = "slink" >docs</a>
+
+<br />
+<a href = "https://sites.google.com/a/tritechsc.org/davidv/" target = "_blank"><img src = "img/grysq.png"></a>
+<a href = "https://github.com/Dave607" target = "_blank"><img src = "img/grysq2.png"></a>
+<img src = "img/grysq3.png">
+
+<br />
+<img src = "img/grysq4.png">
+<img src = "img/grysq5.png">
+<img src = "img/grysq6.png">
+
+<br />
+<img src = "img/grysq7.png">
+<img src = "img/grysq8.png">
+<img src = "img/grysq9.png">
+
+<br />
+<img src="img/dave.jpg" alt="dave.jpg" height="135" width="275">
+
+</body>
+</html>
